403884b78e1fb6bb2a941141f20c55f245674df8
behova <szulauf90@gmail.com>
2022-11-21 13:47:22 -0800

    intial commit

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+
+//load any environment variables from .env into proccess
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@fun-db.hqrvfpp.mongodb.net`;
+
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+
+export const config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    server: {
+        port: SERVER_PORT
+    }
+};
---
+++ src/server.ts
@@ -0,0 +1,71 @@
+import { config } from './config/config';
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import Logging from './library/Logging';
+import authorRoutes from './routes/Author';
+import bookRoutes from './routes/Book';
+
+const router = express();
+
+/** Connect to Mongo */
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('connected');
+        StartServer();
+    })
+    .catch((error) => {
+        Logging.error('Unable to connect:');
+        Logging.error(error);
+    });
+
+/** Only start the server if Mongo connects */
+const StartServer = () => {
+    router.use((req, res, next) => {
+        /** Log the request */
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /** Log the Response */
+            Logging.info(`Outgoing -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Rules of API */
+    router.use((req, res, next) => {
+        /**access ips here */
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+    router.use('/authors', authorRoutes);
+    router.use('/books', bookRoutes);
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+
+        return res.status(400).json({ message: error.message });
+    });
+
+    /** Create Server and log connection */
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}...`));
+};
